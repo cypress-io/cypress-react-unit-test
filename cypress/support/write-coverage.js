337192382975b21b2b8e5f3018762cb734457a24
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-01-16 09:35:34 -0500

    chore: use .bin path to istanbul

--- cypress/support/write-coverage.js
+++ cypress/support/write-coverage.js
@@ -2,6 +2,8 @@
 after(function writeCoverage () {
   console.log('writing coverage', window.__coverage__)
   const s = JSON.stringify(window.__coverage__, null, 2)
+  cy.log('saving coverage.json')
   cy.writeFile('coverage.json', s + '\n\n')
-  cy.exec('npm run coverage-report')
+  cy.log('generate coverage report')
+  cy.exec('./node_modules/.bin/istanbul report --verbose html')
 })
