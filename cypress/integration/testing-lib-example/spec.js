2616a30ea2c9638ddbb8754eb57ed4b95157031a
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-12-24 10:33:35 -0500

    test equivalent to react-testing lib example

---
+++ cypress/integration/testing-lib-example/spec.js
@@ -0,0 +1,17 @@
+/// <reference types="cypress" />
+import React from 'react'
+import { mount } from "cypress-react-unit-tests";
+import Fetch from './fetch'
+
+it('loads and displays greeting', () => {
+  cy.server()
+  cy.route('/greeting', {greeting: 'Hello there'}).as('greet')
+
+  const url = '/greeting'
+  mount(<Fetch url={url} />)
+
+  cy.contains('Load Greeting').click()
+  cy.get('[role=heading]').should('have.text', 'Hello there')
+  cy.get('[role=button]').and('be.disabled')
+  cy.get('@greet').its('url').should('match', /\/greeting$/)
+})
