7f40b07ec48aaef3bc5781589196dda97b28d207
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-01-05 12:43:44 -0500

    format all cypress subfolders

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,6 +1,6 @@
-const path = require("path");
-const webpack = require("@cypress/webpack-preprocessor");
-const babelConfig = require("../../babel.config.js");
+const path = require('path')
+const webpack = require('@cypress/webpack-preprocessor')
+const babelConfig = require('../../babel.config.js')
 
 // should we just reuse root webpack config?
 const webpackOptions = {
@@ -8,41 +8,41 @@ const webpackOptions = {
   // avoid winston logger problem
   // https://github.com/percy/percy-cypress/issues/58
   node: {
-    fs: "empty"
+    fs: 'empty',
   },
   resolve: {
     alias: {
-      react: path.resolve("./node_modules/react")
-    }
+      react: path.resolve('./node_modules/react'),
+    },
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx|mjs)$/,
-        loader: "babel-loader",
-        options: babelConfig
+        loader: 'babel-loader',
+        options: babelConfig,
       },
       {
         test: /\.css$/,
         exclude: [/node_modules/],
-        use: ["style-loader", "css-loader"]
+        use: ['style-loader', 'css-loader'],
       },
       {
         // some of our examples import SVG
         test: /\.svg$/,
-        loader: 'svg-url-loader'
-      }
-    ]
-  }
-};
+        loader: 'svg-url-loader',
+      },
+    ],
+  },
+}
 
 const options = {
   // send in the options from your webpack.config.js, so it works the same
   // as your app's code
   webpackOptions,
-  watchOptions: {}
-};
+  watchOptions: {},
+}
 
 module.exports = on => {
-  on("file:preprocessor", webpack(options));
-};
+  on('file:preprocessor', webpack(options))
+}
