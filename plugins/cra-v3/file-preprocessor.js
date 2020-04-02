75b3d27efadfcc0ac8e20263ab585140871b4af6
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-04-02 18:14:54 -0400

    feat: set cy and Cypress global automatically

--- plugins/cra-v3/file-preprocessor.js
+++ plugins/cra-v3/file-preprocessor.js
@@ -22,10 +22,36 @@ const webpackOptions = webpackFactory('development')
 debug('webpack options: %o', webpackOptions)
 
 // remove bunch of options, we just need to bundle spec files
-// delete webpackOptions.entry
-// delete webpackOptions.output
-// delete webpackOptions.node
 delete webpackOptions.optimization
+delete webpackOptions.plugins
+
+// ESLint loader does not know about our "cy" global so it will error
+// find it in the module processing rules and add global "cy" option
+debug('module property %o', webpackOptions.module)
+
+if (webpackOptions.module && Array.isArray(webpackOptions.module.rules)) {
+  const modulePre = webpackOptions.module.rules.find(rule => rule.enforce === 'pre')
+  if (modulePre && Array.isArray(modulePre.use)) {
+    debug('found Pre block %o', modulePre)
+
+    const useEslintLoader = modulePre.use.find(use => use.loader && use.loader.includes('eslint-loader'))
+    if (useEslintLoader) {
+      debug('found useEslintLoader %o', useEslintLoader)
+
+      if (useEslintLoader.options) {
+        if (Array.isArray(useEslintLoader.options.globals)) {
+          debug('adding cy to existing globals %o', useEslintLoader.options.globals)
+          useEslintLoader.options.globals.push('cy')
+          useEslintLoader.options.globals.push('Cypress')
+        } else {
+          debug('setting new list of globals with cy and Cypress')
+          useEslintLoader.options.globals = ['cy', 'Cypress']
+        }
+      }
+    }
+  }
+}
+
 
 const options = {
   webpackOptions,
