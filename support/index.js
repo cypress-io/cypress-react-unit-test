796d2486ee24b52b797be5325dbac53849d31ca4
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-04-03 13:18:24 -0400

    fix: include dist/hooks

--- support/index.js
+++ support/index.js
@@ -1,3 +1,3 @@
 // should be loaded from cypress.json as a support file
 // "supportFile": "node_modules/cypress-react-unit-test/support"
-import 'cypress-react-unit-test/hooks'
+import 'cypress-react-unit-test/dist/hooks'
