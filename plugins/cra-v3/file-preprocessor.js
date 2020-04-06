ddbdbe2b8bd679eed30f16a5b42c4ed066a4326d
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-04-05 22:47:44 -0400

    feat: use find-webpack to handle ejected react-scripts too

--- plugins/cra-v3/file-preprocessor.js
+++ plugins/cra-v3/file-preprocessor.js
@@ -1,21 +1,6 @@
 const debug = require('debug')('cypress-react-unit-test')
-const path = require('path')
-const findYarnWorkspaceRoot = require('find-yarn-workspace-root')
+const findWebpack = require('find-webpack')
 const webpack = require('@cypress/webpack-preprocessor')
-
-const webpackConfigPath = path.resolve(
-  findYarnWorkspaceRoot() || process.cwd(),
-  'node_modules',
-  'react-scripts',
-  'config',
-  'webpack.config.js',
-)
-
-debug('path to react-scripts own webpack.config.js: %s', webpackConfigPath)
-
-// Do this as the first thing so that any code reading it knows the right env.
-process.env.BABEL_ENV = 'development'
-process.env.NODE_ENV = 'development'
 
 // note: modifies the argument object in place
 const addCypressToEslintRules = webpackOptions => {
@@ -89,8 +74,14 @@ const addCodeCoverage = webpackOptions => {
 const getWebpackOptions = opts => {
   debug('top level opts %o', opts)
 
-  const webpackFactory = require(webpackConfigPath)
-  const webpackOptions = webpackFactory('development')
+  const webpackOptions = findWebpack.getWebpackOptions()
+  if (!webpackOptions) {
+    console.error('⚠️ Could not find Webpack options, using defaults')
+    return {
+      webpackOptions: webpack.defaultOptions,
+      watchOptions: {},
+    }
+  }
   debug('webpack options: %o', webpackOptions)
 
   // remove bunch of options, we just need to bundle spec files
