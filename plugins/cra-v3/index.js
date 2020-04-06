60fa438dc2699a76d107cf5b0e4e48cebf2167f7
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-04-06 16:00:15 -0400

    feat: use new code coverage version

--- plugins/cra-v3/index.js
+++ plugins/cra-v3/index.js
@@ -2,5 +2,8 @@ const filePreprocessor = require('./file-preprocessor')
 
 module.exports = (on, config) => {
   on('file:preprocessor', filePreprocessor(config))
-  on('task', require('@cypress/code-coverage/task'))
+  on('task', require('@cypress/code-coverage/task')(on, config))
+  // IMPORTANT to return the config object
+  // with the any changed environment variables
+  return config
 }
